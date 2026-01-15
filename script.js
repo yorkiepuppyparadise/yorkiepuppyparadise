467a66d90f6ad95b54d54dc27b4d0fbba6ace698
yorkiepuppyparadise <itsjameswilliams68@gmail.com>
2026-01-15 22:15:52 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -306,4 +306,12 @@ window.addEventListener("DOMContentLoaded", () => {
   });
 });
 
+// Smooth scroll when nav links clicked
+document.querySelectorAll('.nav-links a').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    target.scrollIntoView({ behavior: 'smooth' });
+  });
+});
 
