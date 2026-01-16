27b028b056813f54b78acabe4320dc23ed6ef664
yorkiepuppyparadise <itsjameswilliams68@gmail.com>
2026-01-16 01:15:18 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -315,3 +315,12 @@ document.querySelectorAll('.nav-links a').forEach(link => {
   });
 });
 
+document.querySelectorAll('.nav-links a').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    target.scrollIntoView({ behavior: 'smooth' });
+  });
+});
+
+
